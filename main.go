9bf60c66f36c7eaa5f620a699ee88576cd6ec363
Sean Anih <sean.anih@gmail.com>
2023-03-19 21:18:42 -0400

    added /albums/:id api route functionality

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -22,10 +23,43 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
-
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbum)
+	
+	
 	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON "newAlbum"
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// add the album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+	fmt.Println("album posted")
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, value := range albums {
+		if value.ID == id {
+			c.IndentedJSON(http.StatusOK, value)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
